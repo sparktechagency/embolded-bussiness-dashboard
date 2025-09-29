497750f64f58f6eb117bea117a565567a2070c5f
pranab56 <pronabhalder53@gmail.com>
2025-09-29 14:55:45 +0600

    chore: add allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: "217.15.171.166", // allow access from external IP/domains
     port: 4000,      // your port
-
+    allowedHosts: ["business.dutyhourapp.com", "www.business.dutyhourapp.com", "www.api.dutyhourapp.com", "api.dutyhourapp.com"], // 👈 add your domain here
   },
 })
